c51db25f37a4a6b25d942f824cd715483c5749e2
Aleksandra Majewska <01158829@pw.edu.pl>
2021-06-06 21:00:48 +0200

    adding noexcept override to errors

--- crossword/crossword/crosswordErrors.cpp
+++ crossword/crossword/crosswordErrors.cpp
@@ -5,48 +5,48 @@
 class FieldNotSettedUp : public std::exception
 {
 public:
-    virtual char const* what() const { return "This field should remain empty."; }
+    virtual char const* what() const noexcept override { return "This field should remain empty."; }
 };
 
 
 class NotAlphaOrSpace : public std::exception
 {
 public:
-    virtual char const* what() const { return "This field should store alpha or space."; }
+    virtual char const* what() const noexcept override { return "This field should store alpha or space."; }
 };
 
 
 class NotAlpha : public std::exception
 {
 public:
-    virtual char const* what() const { return "This shoule be an alpha."; }
+    virtual char const* what() const noexcept override { return "This shoule be an alpha."; }
 };
 
 
 class NotNumber : public std::exception
 {
 public:
-    virtual char const* what() const { return "This is not a number."; }
+    virtual char const* what() const noexcept override { return "This is not a number."; }
 };
 
 
 class InvalidOrientation : public std::exception
 {
 public:
-    virtual char const* what() const { return "Invalid orientation."; }
+    virtual char const* what() const noexcept override { return "Invalid orientation."; }
 };
 
 
 class BoardSizeException : public std::exception
 {
 public:
-    virtual char const* what() const { return "Invalid board size."; }
+    virtual char const* what() const noexcept override { return "Invalid board size."; }
 };
 
 
 class InvalidData : public std::exception
 {
 public:
-    virtual char const* what() const { return "Invalid input data."; }
+    virtual char const* what() const noexcept override { return "Invalid input data."; }
 };
 
